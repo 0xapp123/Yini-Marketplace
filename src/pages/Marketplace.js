f286126a6b3b39a863e9c42ff492a158d676b803
0xed123 <apollo1030109@gmail.com>
2023-11-22 13:46:50 +0900

    chore: updated latest code

--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -6,35 +6,53 @@ import { CONTRACT_ABI, CONTRACT_ADDRESS } from '../config'
 function Marketplace () {
   const [data, setData] = useState()
   const [mintIds, setMintIds] = useState()
+  const [sort, setSort] = useState(true)
 
   const { address } = useAccount()
 
   const mintedData = useContractRead({
     address: CONTRACT_ADDRESS,
     abi: CONTRACT_ABI,
-    functionName: 'getAllTokens'
+    functionName: 'getAllTokens',
+    watch: true
   })
 
   useEffect(() => {
-    if (mintedData && mintedData.data) {
+    if (mintedData.data !== null || mintedData.data !== undefined) {
       let _mintedIds = []
       mintedData.data.forEach(datum => {
         _mintedIds.push(Number(datum))
       })
       setMintIds(_mintedIds)
     }
-
+    setData([])
     let _data = []
-    for (let i = 499; i >= 0; i--) {
-      _data.push(i)
+    if (sort) {
+      for (let i = 499; i >= 0; i--) {
+        _data.push(i)
+      }
+    } else {
+      for (let i = 0; i <= 499; i++) {
+        _data.push(i)
+      }
     }
     setData(_data)
-  }, [address])
+  }, [address, sort])
 
   return (
     <>
       <div className='mt-20 px-4 lg:px-24'>
-        <h1 className='text-white text-[42px] font-bold '>Marketplace</h1>
+        <div className='flex flex-row justify-between items-center'>
+          <h1 className='text-white text-3xl sm:text-[42px] font-bold '>
+            Marketplace
+          </h1>
+          <div
+            className='cursor-pointer hover:animate-pulse hover:scale-105 transition-all border-[1px] border-solid border-pink-300 bg-gradient-to-r from-pink-500 via-purple-600 to-blue-500 rounded-[5px] py-1 px-6 text-white my-2 text-xl sm:text-2xl font-semibold'
+            onClick={() => setSort(!sort)}
+          >
+            Sort
+          </div>
+        </div>
         <div className='w-full flex justify-center items-center mx-auto'>
           <NftGrid datas={data} mintedIds={mintIds} />
         </div>

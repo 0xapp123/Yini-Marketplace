e45bae88184bfc02e673a3297acd3d0c47a000cd
dev-tom-0108 <shinji.sakai111@gmail.com>
2023-11-23 18:19:03 +0900

    chore: remove unused variable

--- src/hook/useContract.js
+++ src/hook/useContract.js
@@ -3,28 +3,7 @@ import { read, write } from "./utils";
 import { LP_TOKEN_ABI, LP_TOKEN_ADDRESS } from "../config";
 
 export function useContract() {
-  const approve = async (
-    tokenAddress,
-    spender,
-    amount,
-  ) => {
-    return await write({
-      address: CONTRACT_ADDRESS,
-      abi: CONTRACT_ABI,
-      functionName: "approve",
-      args: [spender, amount],
-    });
-  };
-
-  const balanceOf = async (accountAddress) => {
-    return await read({
-      address: CONTRACT_ADDRESS,
-      abi: CONTRACT_ABI,
-      functionName: 'balanceOf',
-      args: [accountAddress]
-    })
-  };
-
+ 
   const getRealPrice = async () => {
     return await read({
       address: CONTRACT_ADDRESS,
@@ -34,8 +13,6 @@ export function useContract() {
   };
 
   return {
-    approve,
-    balanceOf,
     getRealPrice
   };
 }
